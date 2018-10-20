163978331834be0b793d5eb2940a74ff98ae6ffd
Joost Baaij <joost@spacebabies.nl>
2018-10-20 23:10:04 +0200

    require clamd_check

--- lib/ok_computer/clamav.rb
+++ lib/ok_computer/clamav.rb
@@ -1,7 +1,8 @@
 require "ok_computer/clamav/version"
+require "ok_computer/clamav/clamd_check"
 
 module OkComputer
+  # Monitors ClamAV anti-virus.
   module Clamav
-    # Your code goes here...
   end
 end
